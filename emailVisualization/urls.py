130f3d178a90acd15f4bcedc5a122638251826cf
Jimmy Young <yangjufo@gmail.com>
2019-01-01 21:13:48 -0800

    rename directory

--- emailVisualization/urls.py
+++ emailVisualization/urls.py
@@ -1,4 +1,4 @@
-"""emailVisulation URL Configuration
+"""emailVisualization URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
