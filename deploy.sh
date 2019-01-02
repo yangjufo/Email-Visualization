21f765b88321b7197a3c5989a20a0015d27f3301
Jimmy Young <yangjufo@gmail.com>
2019-01-02 05:28:12 +0000

    add deploy file

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source_folder=/home/ubuntu/sites/emails.jianyang995.com
+
+cd $source_folder/Email-Visualization
+git reset *
+git pull
+sed -i 's/DEBUG = True/DEBUG = False/' ./emailVisualization/settings.py 
+
+$source_folder/env/bin/pip install -r requirements.txt
+
+sudo service nginx reload
+$source_folder/env/bin/gunicorn --bind unix:/tmp/emails.jianyang995.com.socket emailVisualization.wsgi:application&
