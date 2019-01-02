af1744ffd30dbbcf917a8be668d0a1d61f2a740c
Jimmy Young <yangjufo@gmail.com>
2019-01-01 21:11:53 -0800

    rename directory

--- emailVisulation/wsgi.py
+++ emailVisulation/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for emailVvisulation project.
+WSGI config for emailVisulation project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emailVvisulation.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emailVisulation.settings')
 
 application = get_wsgi_application()
